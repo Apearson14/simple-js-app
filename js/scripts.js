b839efca463d75ed7a5154b58fd8c9d487c547bb
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2023-04-02 17:07:18 -0400

    Adding pokemonList variable with first 4 objects
    Charzard, Jigglypuff, Ditto, and Mewtwo

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood='Indian Food'
-document.write(favoriteFood)
+let pokemonList=[
+    {name:'Charizard',height:1.7,type:'Fire'},
+    {name:'Jigglypuff',height:0.5,type:'Fairy'},
+    {name:'Ditto',height:0.3,type:'normal'},
+    {name:'Mewtwo',height:2,type:'psychic'}
+]
