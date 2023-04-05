85f991662a1a1b281034c6d1631a2dc591900043
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2023-04-05 19:34:08 -0400

    Adding a loop to list the pokemon name and hieght

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,6 @@ let pokemonList=[
     {name:'Ditto',height:0.3,type:'normal'},
     {name:'Mewtwo',height:2,type:'psychic'}
 ]
+for(let i=0;i<pokemonList.length; i++) {
+    document.write(pokemonList[i].name + '(height:'+ pokemonList.height+')')
+}
