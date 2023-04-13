1e2e1efeaf86938ba70b9f5af4c632abf9f3f064
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2023-04-13 15:08:51 -0400

    Used a Repl to help trouble shoot this issue then copy and pasted all the code
    I wasn't able to get it to print out on the screen with the IIFE in place. After realizing I wasn't using the getAll object correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,30 @@ let pokemonList=[
     {name:'Jigglypuff',height:0.5,type:'Fairy'},
     {name:'Ditto',height:0.3,type:'normal'},
     {name:'Mewtwo',height:2,type:'psychic'}
-]
-pokemonList.forEach(pokemon => {
-    if (pokemon.height > 1) {
-      document.write(`${pokemon.name} (height: ${pokemon.height}) Wow, that's Big!<br>`);
-    } else {
-      document.write(`${pokemon.name} (height: ${pokemon.height})<br>`);
+];
+
+let pokemonRepository = (function () {
+    let pokemonList = [];
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-  });
-  
+    function getAll() {
+        return pokemonList;
+    }
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+pokemonList.forEach(function(pokemon) {
+    pokemonRepository.add(pokemon);
+});
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    if (pokemon.height > 1) {
+        document.write(`${pokemon.name} (height: ${pokemon.height}) Wow, that's Big!<br>`);
+    } else {
+        document.write(`${pokemon.name} (height: ${pokemon.height})<br>`);
+    }
+});
