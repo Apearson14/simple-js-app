5e22fc59e8d194760887dcb3168ac84f469b9627
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2023-04-13 14:05:18 -0400

    Replacing the for function with foreach function

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,11 @@ let pokemonList=[
     {name:'Ditto',height:0.3,type:'normal'},
     {name:'Mewtwo',height:2,type:'psychic'}
 ]
-for(let i=0;i<pokemonList.length; i++) {
-    if (pokemonList[i].height >1){
-        document.write(pokemonList[i].name + ' ' + "(height:" + pokemonList[i].height + ')' + " " + "Wow, that's Big!" + "<br>");
-    } else{document.write(pokemonList[i].name + ' ' + "(height:" + pokemonList[i].height + ")<br>");
+pokemonList.forEach(pokemon => {
+    if (pokemon.height > 1) {
+      document.write(`${pokemon.name} (height: ${pokemon.height}) Wow, that's Big!<br>`);
+    } else {
+      document.write(`${pokemon.name} (height: ${pokemon.height})<br>`);
     }
-}
+  });
+  
