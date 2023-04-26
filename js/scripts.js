50984d86b89a9c4498caba2762a542b5be0d4222
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2023-04-26 17:41:52 -0400

    Adding Pokemon URL

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
-        {name:'Charizard',height:1.7,type:'Fire'},
-        {name:'Jigglypuff',height:0.5,type:'Fairy'},
-        {name:'Ditto',height:0.3,type:'normal'},
-        {name:'Mewtwo',height:2,type:'psychic'}
-    ];
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
     function add(pokemon) {
         pokemonList.push(pokemon);
     }
