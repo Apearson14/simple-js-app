3d045defd6b1f4baf312ad5d149e8c825ec2eb97
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2023-05-01 16:46:10 -0400

    Removing List functions adding Modal

--- js/scripts.js
+++ js/scripts.js
@@ -18,68 +18,11 @@ let pokemonRepository = (function () {
     function showDetails(pokemon) {
         console.log(pokemon)
     }
-
-    function addListItem(pokemon) {
-        let pokemonList = document.querySelector(".pokemon-list");
-        let listpokemon = document.createElement("li");
-        let button = document.createElement("button");
-        button.innerText = pokemon.name;
-        button.classList.add("button-class");
-        listpokemon.appendChild(button);
-        pokemonList.appendChild(listpokemon);
-        //Adding event listener to each list item
-        button.addEventListener("click", function() {
-            showDetails(pokemon);
-        })
-    }
-
-    function loadList() {
-        return fetch(apiUrl).then(function (response) {
-          return response.json();
-        }).then(function (json) {
-          json.results.forEach(function (item) {
-            let pokemon = {
-              name: item.name,
-              detailsUrl: item.url
-            };
-            add(pokemon);
-            console.log(pokemon);
-          });
-        }).catch(function (e) {
-          console.error(e);
-        })
+    function showModal() {
+        let modalContainer = document.querySelector('#modal-container');
+        modalContainer.classList.add('is-visible');
       }
-      function loadDetails(item) {
-        let url = item.detailsUrl;
-        return fetch(url).then(function (response) {
-          return response.json();
-        }).then(function (details) {
-          // Now we add the details to the item
-          item.imageUrl = details.sprites.front_default;
-          item.height = details.height;
-          item.types = details.types;
-        }).catch(function (e) {
-          console.error(e);
-        });
-      }
-      function showDetails(item) {
-        pokemonRepository.loadDetails(item).then(function () {
-          console.log(item);
-        });
-      }
-    return {
-        add: add,
-        getAll: getAll,
-        addListItem: addListItem,
-        loadList: loadList,
-        loadDetails: loadDetails,
-        showDetails: showDetails
-    };
-})();
-
-pokemonRepository.loadList().then(function () {
-    pokemonRepository.getAll().forEach(function (pokemon) {
-      pokemonRepository.addListItem(pokemon);
-    });
-  });
-
+      
+      document.querySelector('#show-modal').addEventListener('click', () => {
+        showModal();
+      });
