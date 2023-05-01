5e49fae3de80d9040a23647842c76c77b81b92a9
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2023-05-01 17:59:33 -0400

    Adding Modal Close button

--- js/scripts.js
+++ js/scripts.js
@@ -18,11 +18,52 @@ let pokemonRepository = (function () {
     function showDetails(pokemon) {
         console.log(pokemon)
     }
-    function showModal() {
+    function showModal(title,text) {
         let modalContainer = document.querySelector('#modal-container');
+        modalContainer.innerHTML = '';
+        let modal = document.createElement('div');
+        modal.classList.add('modal'); 
+        
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement = document.createElement ('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+
+        titleElement = document.createElement('h1');
+        titleElement.innerText = Title
+
+        let contentElement = document.createElement('p');
+        contentElement.innerText = text;
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modalContainer.appendChild(modal);
         modalContainer.classList.add('is-visible');
       }
       
+      function hideModal() {
+        let modalContainer = document.querySelector('#modal-container');
+        modalContainer.classList.remove('is-visible');
+      }
+
+      window.addEventListener('keydown', (e) => {
+        let modalContainer = document.querySelector('#modal-container');
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+          hideModal();  
+        }
+      });
+
+      let modalContainer = document.querySelector('#modal-container');
+      modalContainer.addEventListener('click', (e) => {
+        // Since this is also triggered when clicking INSIDE the modal
+        // We only want to close if the user clicks directly on the overlay
+        let target = e.target;
+        if (target === modalContainer) {
+          hideModal();
+        }
+      });
+
       document.querySelector('#show-modal').addEventListener('click', () => {
         showModal();
       });
